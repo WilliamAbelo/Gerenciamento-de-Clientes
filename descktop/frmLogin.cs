f2f943d654d9e68ca6c081de692cad4ead019884
William Abelo <william.abelo@outlook.com>
2018-04-05 11:05:39 -0300

    Primeiro Commit
    Inicio do projeto

---
+++ descktop/frmLogin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace descktop
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Connection String
+        //string cs = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\MyDatabase.mdf;Integrated Security=True;";
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "" || txtSenha.Text == "")
+            {
+                MessageBox.Show("Por Favor, Insira um Usuario e Senha");
+                return;
+            }
+            try
+            {
+                //Create SqlConnection
+                //SqlConnection con = new SqlConnection(cs);
+                //SqlCommand cmd = new SqlCommand("Select * from tbl_Login where UserName=@username and Password=@password", con);
+                //cmd.Parameters.AddWithValue("@username", txtUsuario.Text);
+                //cmd.Parameters.AddWithValue("@password", txtSenha.Text);
+                //con.Open();
+                //SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                //DataSet ds = new DataSet();
+                //adapt.Fill(ds);
+                //con.Close();
+                //int count = ds.Tables[0].Rows.Count;
+                //If count is equal to 1, than show frmMain form
+                if (txtUsuario.Text == "will" || txtSenha.Text == "123")
+                {
+                    //MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    frmMain fm = new frmMain();
+                    fm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ descktop/frmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace descktop
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            listRegioes.Items.Add("Regiao 1");
+            listRegioes.Items.Add("Regiao 2");
+            listRegioes.Items.Add("Regiao 3");
+            listRegioes.Items.Add("Regiao 4");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmLogin fl = new frmLogin();
+            fl.Show();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listRegioes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selecionado = listRegioes.SelectedItem;
+
+            switch (listRegioes.SelectedItem)
+            {
+                case "Regiao 1":
+
+                    break;
+                case "Regiao 2":
+
+                    break;
+                case "Regiao 3":
+
+                    break;
+                case "Regiao 4":
+
+                    break;
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmUsuario fm = new frmUsuario();
+            fm.Show();
+        }
+    }
+}
